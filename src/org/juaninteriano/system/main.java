d10f7a67d4134309ec67bbe36ae000ee5015b7d9
Informatica <informatica@alumno.local>
2026-06-12 11:51:02 -0600

    000 Primer commit

---
+++ src/org/juaninteriano/system/main.java
@@ -0,0 +1,30 @@
+package org.juaninteriano.system;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+
+
+
+
+
+public class main extends Application{
+
+
+    @Override
+    public void start(Stage escenarioPrincipal) throws Exception {
+      //nodos
+        VBox raiz = new VBox();
+        //escena
+        Scene escena = new Scene(raiz);
+        //cargar escena y mostrar escenario principal
+        escenarioPrincipal.setTitle("Calculadora de Esteban");
+        escenarioPrincipal.setScene(escena);
+        escenarioPrincipal.show();      
+    }
+    public static void main(String[] args) {
+        Application.launch(args);
+}
+}
